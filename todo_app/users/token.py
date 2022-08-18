3d3a585edbfd86c03687d70c69bcbbdfade9cc32
Adebiyi Oluwafemi <horlarmihleykan10@gmail.com>
2022-08-18 01:46:22 +0100

    Update token.py

--- todo_app/users/token.py
+++ todo_app/users/token.py
@@ -1,18 +1,16 @@
 from todo_app import db,login_manager,app
-
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeSerializer
 from todo_app.models import User
 
+def get_token(self):
+    s = URLSafeSerializer(app.config['SECRET_KEY'])
+    return s.dumps({'user_id': self.id})
 
-def get_token(self, expires_sec=600):
-    s = Serializer(app.config['SECRET_KEY'], expires_sec)
-    return s.dumps({'user_id': self.id}).decode('utf-8')
-
-# @staticmethod
-def verify_token(token):
-    s = Serializer(app.config['SECRET_KEY'])
+#@staticmethod
+def verify_token(token, expiration=120):
+    s = URLSafeSerializer(app.config['SECRET_KEY'])
     try:
-        user_id = s.loads(token)['user_id']
+        user_id = s.loads(token,max_age=expiration)['user_id']
     except:
         return None
     return User.query.get(user_id)
